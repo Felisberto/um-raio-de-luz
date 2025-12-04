d420f1b650ca40a8edc618e91d7d94d7d86bf4e5
Felipe Barros <90708370+Felisberto@users.noreply.github.com>
2025-12-04 14:29:31 -0300

    Delete client directory

--- client/src/components/examples/StarryBackground.tsx
+++
@@ -1,5 +0,0 @@
-import StarryBackground from '../StarryBackground';
-
-export default function StarryBackgroundExample() {
-  return <StarryBackground />;
-}
--- client/src/pages/examples/RomanticLetter.tsx
+++
@@ -1,5 +0,0 @@
-import RomanticLetter from '../RomanticLetter';
-
-export default function RomanticLetterExample() {
-  return <RomanticLetter />;
-}
